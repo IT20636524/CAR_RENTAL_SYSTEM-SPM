8a21f232b0882ecd4071f77b0e99f25bb1f3f204
B.M.N.D.S.Ariyarathnna <it20643836@my.sliit.lk>
2022-08-19 19:14:55 +0530

    Home page modified

---
+++ car-rental-system/client/src/components/Footer.tsx
@@ -0,0 +1,145 @@
+import React from 'react';
+import {
+  MDBFooter,
+  MDBContainer,
+  MDBIcon,
+  MDBCol,
+  MDBRow,
+  MDBRipple
+} from 'mdb-react-ui-kit';
+
+export default function Footer() {
+  return (
+    <MDBFooter className='text-center text-white' style={{ backgroundColor: '#caced1' }}>
+      <MDBContainer className='p-4'>
+        <section className=''>
+          <MDBRow>
+            <MDBCol lg='2' md='12' className='mb-4 mb-md-0'>
+              <MDBRipple
+                rippleColor='light'
+                className='bg-image hover-overlay shadow-1-strong rounded'
+              >
+                <img style={{"height":"130px"}} src='https://assets.entrepreneur.com/content/3x2/2000/20170114111605-car.jpeg' className='w-100' alt='' />
+                <a href='#!'>
+                  <div
+                    className='mask'
+                    style={{ backgroundColor: 'rgba(251, 251, 251, 0.2)' }}
+                  ></div>
+                </a>
+              </MDBRipple>
+            </MDBCol>
+            <MDBCol lg='2' md='12' className='mb-4 mb-md-0'>
+              <MDBRipple
+                rippleColor='light'
+                className='bg-image hover-overlay shadow-1-strong rounded'
+              >
+                <img style={{"height":"130px"}} src='https://thumbs.dreamstime.com/b/car-rent-sale-agent-auto-dealer-leasing-concept-104364904.jpg' className='w-100' alt='' />
+                <a href='#!'>
+                  <div
+                    className='mask'
+                    style={{ backgroundColor: 'rgba(251, 251, 251, 0.2)' }}
+                  ></div>
+                </a>
+              </MDBRipple>
+            </MDBCol>
+            <MDBCol lg='2' md='12' className='mb-4 mb-md-0'>
+              <MDBRipple
+                rippleColor='light'
+                className='bg-image hover-overlay shadow-1-strong rounded'
+              >
+                <img style={{"height":"130px"}} src='https://media.istockphoto.com/photos/male-hand-holding-electronic-remote-key-pushing-button-near-red-car-picture-id1369366944?b=1&k=20&m=1369366944&s=170667a&w=0&h=C2Gw5ScNSyX4MNB2UFuPlyNbG3hSWaAN4f5CsjuxtQc=' className='w-100' alt=''/>
+                <a href='#!'>
+                  <div
+                    className='mask'
+                    style={{ backgroundColor: 'rgba(251, 251, 251, 0.2)' }}
+                  ></div>
+                </a>
+              </MDBRipple>
+            </MDBCol>
+            <MDBCol lg='2' md='12' className='mb-4 mb-md-0'>
+              <MDBRipple
+                rippleColor='light'
+                className='bg-image hover-overlay shadow-1-strong rounded'
+              >
+                <img style={{"height":"130px"}} src='https://clark.com/wp-content/uploads/2021/06/car-rental-save-money.jpg' className='w-100' alt='' />
+                <a href='#!'>
+                  <div
+                    className='mask'
+                    style={{ backgroundColor: 'rgba(251, 251, 251, 0.2)' }}
+                  ></div>
+                </a>
+              </MDBRipple>
+            </MDBCol>
+            <MDBCol lg='2' md='12' className='mb-4 mb-md-0'>
+              <MDBRipple
+                rippleColor='light'
+                className='bg-image hover-overlay shadow-1-strong rounded'
+              >
+                <img style={{"height":"130px"}} src='https://images.unsplash.com/photo-1565043666747-69f6646db940?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2FyJTIwcmVudGFsfGVufDB8fDB8fA%3D%3D&w=1000&q=80' className='w-100' alt='' />
+                <a href='#!'>
+                  <div
+                    className='mask'
+                    style={{ backgroundColor: 'rgba(251, 251, 251, 0.2)' }}
+                  ></div>
+                </a>
+              </MDBRipple>
+            </MDBCol>
+            <MDBCol lg='2' md='12' className='mb-4 mb-md-0'>
+              <MDBRipple
+                rippleColor='light'
+                className='bg-image hover-overlay shadow-1-strong rounded'
+              >
+                <img style={{"height":"130px"}} src='https://media-cldnry.s-nbcnews.com/image/upload/newscms/2019_13/2798361/190325-rental-cars-cs-229p.jpg' className='w-100' alt='' />
+                <a href='#!'>
+                  <div
+                    className='mask'
+                    style={{ backgroundColor: 'rgba(251, 251, 251, 0.2)' }}
+                  ></div>
+                </a>
+              </MDBRipple>
+            </MDBCol>
+          </MDBRow>
+        </section>
+
+        <section className='mb-4 mt-4'>
+         <a className='btn btn-outline-light btn-floating m-1' href='#!' role='button'>
+           <MDBIcon fab icon='facebook-f' />
+         </a>
+
+         <a className='btn btn-outline-light btn-floating m-1' href='#!' role='button'>
+           <MDBIcon fab icon='twitter' />
+         </a>
+
+         <a className='btn btn-outline-light btn-floating m-1' href='#!' role='button'>
+           <MDBIcon fab icon='google' />
+         </a>
+         <a className='btn btn-outline-light btn-floating m-1' href='#!' role='button'>
+           <MDBIcon fab icon='instagram' />
+         </a>
+
+         <a className='btn btn-outline-light btn-floating m-1' href='#!' role='button'>
+           <MDBIcon fab icon='linkedin-in' />
+         </a>
+
+         <a className='btn btn-outline-light btn-floating m-1' href='#!' role='button'>
+           <MDBIcon fab icon='github' />
+         </a>
+       </section>
+      </MDBContainer>
+
+      <div className='text-center p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
+        © 2020 Copyright:
+        <a className='text-white' href='https://gorent.com/'>
+          GoRent.com
+        </a>
+      </div>
+    </MDBFooter>
+    
+    
+     
+   
+
+   
+  
+  );
+}
