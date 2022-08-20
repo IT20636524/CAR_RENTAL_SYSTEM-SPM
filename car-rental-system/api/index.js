81dc32119ef2d4649b7dd1d448cefaaafd3209a5
IT20653118 <it20653118@my.sliit.lk>
2022-08-20 10:08:21 +0530

    Add Car Route updated

--- car-rental-system/api/index.js
+++ car-rental-system/api/index.js
@@ -3,8 +3,12 @@ const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const UserAuthRoute = require("./routes/UserAuth");
+
 const CardRoute = require("./routes/cards");
 const StaffRoute = require("./routes/staff");
+
+const carRoute = require("./routes/cars");
+
 
 const cors=require("cors");
 
@@ -28,8 +32,12 @@ mongoose.connect(process.env.MONGO_URL,{
 .catch(err=>console.log(err));
 
 app.use("/api/UserAuth", UserAuthRoute);
+
 app.use("/api/cards", CardRoute);
 app.use("/api/staff", StaffRoute);
+
+app.use("/api/cars", carRoute);
+
 
 app.listen("5000", ()=>{
     console.log("Backend is running.");
