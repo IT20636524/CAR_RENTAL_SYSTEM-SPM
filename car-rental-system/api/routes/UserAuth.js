ae8252c31152a9c6d430b62cd96f769c8f7b66f1
PrathibhaSathyanjalee <88728787+IT20636524@users.noreply.github.com>
2022-08-20 20:13:19 +0530

    User Management Backend-Get user

--- car-rental-system/api/routes/UserAuth.js
+++ car-rental-system/api/routes/UserAuth.js
@@ -44,4 +44,15 @@ router.post("/login", async(req,res) => {
     }
 });
 
+//Get users
+router.get("/:user_id", async(req,res) => {
+    try{
+        const user = await User.findOne({ 'user_id':req.params.user_id });
+        const {password, ...others} = user._doc;
+        res.status(200).json(others);
+    }catch(err){
+        res.status(500).json(err);
+    }
+});
+
 module.exports = router;
