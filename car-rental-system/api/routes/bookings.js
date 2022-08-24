083f21cca8bfd49db2b700400a9d9c6bfda04be2
PrathibhaSathyanjalee <88728787+IT20636524@users.noreply.github.com>
2022-08-25 01:03:07 +0530

    Booking Management Backend-Update Booking

--- car-rental-system/api/routes/bookings.js
+++ car-rental-system/api/routes/bookings.js
@@ -43,4 +43,18 @@ router.get("/:booking_id", async(req,res) => {
     }
 });
 
+//Update booking
+router.put("/update/:id", async(req,res) => {
+    try{
+        const updatedBooking = await Booking.findOneAndUpdate({'booking_id':req.params.id},
+            {
+                $set:req.body
+            },{new:true}
+        );
+        res.status(200).json(updatedBooking);
+    }catch(err){
+        res.status(500).json(err);
+    }
+});
+
 module.exports = router;
