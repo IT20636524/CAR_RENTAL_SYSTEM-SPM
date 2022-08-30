e079f3c4de7c5e7fd5db04fd195e6543fbb56dd6
B.M.N.D.S.Ariyarathnna <it20643836@my.sliit.lk>
2022-08-30 11:31:09 +0530

    Add Payment page frontend done

--- car-rental-system/client/src/App.tsx
+++ car-rental-system/client/src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 // import './App.scss';
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import AddPayment from './pages/AddPaymentPage';
 import AdminStaffPage from './pages/AdminStaffPage';
 import Dashboard from './pages/Dashboard';
 
 import Homepage from './pages/Home';
+import PaymentView from './pages/PaymentViewPage';
 
 
 
@@ -19,6 +21,8 @@ function App() {
           <Route path="/" element={<Homepage />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/adminstaffpage" element={<AdminStaffPage />} />
+           <Route path="/add-payment" element={<AddPayment />} />
+          <Route path="/paymentview/:payment_id" element={<PaymentView />} />
           
         
         
