850301f175339a6706f04f4c3f86421ceb647ecd
PrathibhaSathyanjalee <IT20636524@my.sliit.lk>
2022-09-09 01:28:42 -1200

    Add Booking Page Connect to Backendend

--- car-rental-system/client/src/pages/AddBooking.tsx
+++ car-rental-system/client/src/pages/AddBooking.tsx
@@ -1,10 +1,12 @@
-import React from "react";
+import axios from 'axios'
+import React, { useState } from 'react'
 import AddBookingForm from "../components/AddBookingForm";
 import BookingSidebar from "../components/BookingSidebar";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 
 export default function AddBooking(){
+    
     return(
         <>
             <Header/>
