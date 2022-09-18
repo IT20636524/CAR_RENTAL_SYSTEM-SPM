bf8edafd1da1cf9f994198eb4a35fcf77eb9eef1
IT20653118 <it20653118@my.sliit.lk>
2022-09-18 19:00:11 +0530

    Vehicle Fleet 1st update

--- car-rental-system/client/src/pages/VehicleFleet.tsx
+++ car-rental-system/client/src/pages/VehicleFleet.tsx
@@ -5,12 +5,219 @@ import Header from '../components/Header'
 export default function VehicleFleet() {
   return (
     <div>
-        <Header />
-        <br /> <br />
-            <h3>Luxury Cars</h3>
-            <h3>Premium Cars</h3>
-            <h3>General Cars</h3>
-        <Footer />
+      <Header />
+      <br /> <br />
+      <h3 style={{ "marginLeft": "15px"}}>Luxury Cars</h3>
+      <div className="row row-cols-1 row-cols-md-6 g-2" style={{ "marginLeft": "10px", "marginRight" : "10px"}}>
+        <div className="col">
+          <div className="card" >
+            <img src="https://mdbcdn.b-cdn.net/img/new/standard/nature/184.webp" className="card-img-top" alt="Fissure in Sandstone" />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#!" className="btn btn-primary">Button</a>
+            </div>
+          </div>
+        </div>
+
+        <div className="col">
+          <div className="card">
+            <img src="https://mdbcdn.b-cdn.net/img/new/standard/nature/184.webp" className="card-img-top" alt="Fissure in Sandstone" />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#!" className="btn btn-primary">Button</a>
+            </div>
+          </div>
+        </div>
+
+        <div className="col">
+          <div className="card">
+            <img src="https://mdbcdn.b-cdn.net/img/new/standard/nature/184.webp" className="card-img-top" alt="Fissure in Sandstone" />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#!" className="btn btn-primary">Button</a>
+            </div>
+          </div>
+        </div>
+
+        <div className="col">
+          <div className="card">
+            <img src="https://mdbcdn.b-cdn.net/img/new/standard/nature/184.webp" className="card-img-top" alt="Fissure in Sandstone" />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#!" className="btn btn-primary">Button</a>
+            </div>
+          </div>
+        </div>
+
+        <div className="col">
+          <div className="card">
+            <img src="https://mdbcdn.b-cdn.net/img/new/standard/nature/184.webp" className="card-img-top" alt="Fissure in Sandstone" />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#!" className="btn btn-primary">Button</a>
+            </div>
+          </div>
+        </div>
+
+        <div className="col">
+          <div className="card">
+            <img src="https://mdbcdn.b-cdn.net/img/new/standard/nature/184.webp" className="card-img-top" alt="Fissure in Sandstone" />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#!" className="btn btn-primary">Button</a>
+            </div>
+          </div>
+        </div>
+        
+      </div>
+      <br/>
+      <h3 style={{ "marginLeft": "15px"}}>Premium Cars</h3>
+      <div className="row row-cols-1 row-cols-md-6 g-2" style={{ "marginLeft": "10px", "marginRight" : "10px"}}>
+        <div className="col">
+          <div className="card" >
+            <img src="https://mdbcdn.b-cdn.net/img/new/standard/nature/184.webp" className="card-img-top" alt="Fissure in Sandstone" />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#!" className="btn btn-primary">Button</a>
+            </div>
+          </div>
+        </div>
+
+        <div className="col">
+          <div className="card">
+            <img src="https://mdbcdn.b-cdn.net/img/new/standard/nature/184.webp" className="card-img-top" alt="Fissure in Sandstone" />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#!" className="btn btn-primary">Button</a>
+            </div>
+          </div>
+        </div>
+
+        <div className="col">
+          <div className="card">
+            <img src="https://mdbcdn.b-cdn.net/img/new/standard/nature/184.webp" className="card-img-top" alt="Fissure in Sandstone" />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#!" className="btn btn-primary">Button</a>
+            </div>
+          </div>
+        </div>
+
+        <div className="col">
+          <div className="card">
+            <img src="https://mdbcdn.b-cdn.net/img/new/standard/nature/184.webp" className="card-img-top" alt="Fissure in Sandstone" />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#!" className="btn btn-primary">Button</a>
+            </div>
+          </div>
+        </div>
+
+        <div className="col">
+          <div className="card">
+            <img src="https://mdbcdn.b-cdn.net/img/new/standard/nature/184.webp" className="card-img-top" alt="Fissure in Sandstone" />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#!" className="btn btn-primary">Button</a>
+            </div>
+          </div>
+        </div>
+
+        <div className="col">
+          <div className="card">
+            <img src="https://mdbcdn.b-cdn.net/img/new/standard/nature/184.webp" className="card-img-top" alt="Fissure in Sandstone" />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#!" className="btn btn-primary">Button</a>
+            </div>
+          </div>
+        </div>
+        
+      </div>
+      <br/>
+      <h3 style={{ "marginLeft": "15px"}}>General Cars</h3>
+      <div className="row row-cols-1 row-cols-md-6 g-2" style={{ "marginLeft": "10px", "marginRight" : "10px"}}>
+        <div className="col">
+          <div className="card" >
+            <img src="https://mdbcdn.b-cdn.net/img/new/standard/nature/184.webp" className="card-img-top" alt="Fissure in Sandstone" />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#!" className="btn btn-primary">Button</a>
+            </div>
+          </div>
+        </div>
+
+        <div className="col">
+          <div className="card">
+            <img src="https://mdbcdn.b-cdn.net/img/new/standard/nature/184.webp" className="card-img-top" alt="Fissure in Sandstone" />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#!" className="btn btn-primary">Button</a>
+            </div>
+          </div>
+        </div>
+
+        <div className="col">
+          <div className="card">
+            <img src="https://mdbcdn.b-cdn.net/img/new/standard/nature/184.webp" className="card-img-top" alt="Fissure in Sandstone" />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#!" className="btn btn-primary">Button</a>
+            </div>
+          </div>
+        </div>
+
+        <div className="col">
+          <div className="card">
+            <img src="https://mdbcdn.b-cdn.net/img/new/standard/nature/184.webp" className="card-img-top" alt="Fissure in Sandstone" />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#!" className="btn btn-primary">Button</a>
+            </div>
+          </div>
+        </div>
+
+        <div className="col">
+          <div className="card">
+            <img src="https://mdbcdn.b-cdn.net/img/new/standard/nature/184.webp" className="card-img-top" alt="Fissure in Sandstone" />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#!" className="btn btn-primary">Button</a>
+            </div>
+          </div>
+        </div>
+
+        <div className="col">
+          <div className="card">
+            <img src="https://mdbcdn.b-cdn.net/img/new/standard/nature/184.webp" className="card-img-top" alt="Fissure in Sandstone" />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#!" className="btn btn-primary">Button</a>
+            </div>
+          </div>
+        </div>
+        
+      </div>
+      <br/> <br/>
+      <Footer />
     </div>
   )
 }
