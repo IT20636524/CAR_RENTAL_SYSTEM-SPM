cfac53ff6df3e56bd4316199fd8a1e853c68cb79
PrathibhaSathyanjalee <IT20636524@my.sliit.lk>
2022-09-18 01:31:54 -1200

    Update Search Booking

--- car-rental-system/client/src/components/BookingModal/SearchBookingModal.tsx
+++ car-rental-system/client/src/components/BookingModal/SearchBookingModal.tsx
@@ -8,7 +8,7 @@ export default function SearchBookingModal() {
             <>
                 <Modal.Header>
                     <div className="col-md-12 text-center">
-                        <Modal.Title id="contained-modal-title-vcenter">Student Details</Modal.Title>
+                        <Modal.Title id="contained-modal-title-vcenter">Booking Details</Modal.Title>
                     </div>
                 </Modal.Header>
                 <Modal.Body>
@@ -18,7 +18,7 @@ export default function SearchBookingModal() {
                             <Form.Group as={Col} className="p-2 bd-highlight">
                                 <Col>
                                     <Form.Label as="legend" column sm={2}>
-                                        Student_Image
+                                        Vehicle_Image
                                     </Form.Label>
                                 </Col>
 
@@ -32,75 +32,75 @@ export default function SearchBookingModal() {
 
                         <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
                             <Form.Label column sm="2">
-                                Full Name
+                                Booking id
                             </Form.Label>
                             <Col sm="7">
-                                <Form.Control type="text" placeholder="Enter your Full Name "  />
+                                <Form.Control type="text" placeholder="Booking Id "  />
                             </Col>
                         </Form.Group>
 
                         <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
                             <Form.Label column sm="2">
-                                Age
+                                Name
                             </Form.Label>
-                            <Col sm="5">
-                                <Form.Control type="text" placeholder="Enter your age" />
+                            <Col sm="7">
+                                <Form.Control type="text" placeholder="Name" />
                             </Col>
                         </Form.Group>
 
                         <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
-                            <Form.Label column sm="2">
-                                Gender
-                            </Form.Label>
-                            <Col sm="5">
-                                <Form.Control type="text" placeholder="Enter your gender"  />
-                            </Col>
-                        </Form.Group>
-
-                        <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
-                            <Form.Label column sm="2">
-                                School
-                            </Form.Label>
-                            <Col sm="7">
-                                <Form.Control type="text" placeholder="Enter your School" />
-                            </Col>
-                        </Form.Group>
-
-
-                        <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
-                            <Form.Label column sm="2">
-                                Email Address
-                            </Form.Label>
-                            <Col sm="7">
-                                <Form.Control type="Email" placeholder="Enter your  Email Address"  />
-                            </Col>
-                        </Form.Group>
-
-
-                        <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
                             <Form.Label column sm="2">
                                 Address
                             </Form.Label>
                             <Col sm="7">
-                                <Form.Control type="textarea" placeholder="Enter your Address"/>
+                                <Form.Control type="text" placeholder="Address"  />
                             </Col>
                         </Form.Group>
 
                         <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
                             <Form.Label column sm="2">
-                                Phone Number
+                                Contact Number
                             </Form.Label>
                             <Col sm="7">
-                                <Form.Control type="tel" placeholder="Enter your Phone Number" maxLength={10} />
+                                <Form.Control type="text" placeholder="Contact Number" maxLength={10} />
+                            </Col>
+                        </Form.Group>
+
+
+                        <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
+                            <Form.Label column sm="2">
+                                Type Of Service
+                            </Form.Label>
+                            <Col sm="7">
+                                <Form.Control type="text" placeholder="Type Of Service"  />
+                            </Col>
+                        </Form.Group>
+
+
+                        <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
+                            <Form.Label column sm="2">
+                                Selected Model
+                            </Form.Label>
+                            <Col sm="7">
+                                <Form.Control type="text" placeholder="Selected Model"/>
+                            </Col>
+                        </Form.Group>
+
+                        <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
+                            <Form.Label column sm="2">
+                                No of Days
+                            </Form.Label>
+                            <Col sm="7">
+                                <Form.Control type="text" placeholder="No of Days" />
                             </Col>
                         </Form.Group>
 
                         <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
                             <Form.Label column sm="2">
-                                Grade
+                                Location
                             </Form.Label>
                             <Col sm="7">
-                                <Form.Control type="text" placeholder="Enter your Grade"/>
+                                <Form.Control type="text" placeholder="Location"/>
                             </Col>
                         </Form.Group>
 
--- car-rental-system/client/src/components/SearchBooking.tsx
+++ car-rental-system/client/src/components/SearchBooking.tsx
@@ -2,11 +2,12 @@ import React, { useState } from 'react'
 import { MDBCol, MDBFormInline, MDBIcon } from "mdbreact";
 import { Button, Modal } from 'react-bootstrap';
 import './styles.css'
+import SearchBookingModal from './BookingModal/SearchBookingModal';
 
 const SearchBooking = (props: any) => {
 
     const [show, setShow] = useState(false);
-    const [studentId, setStudentId] = useState(false);
+    const [bookingId, setBookingId] = useState("");
     const handleClose = () => setShow(false);
 
     function searchId(e:any) {
@@ -21,13 +22,14 @@ const SearchBooking = (props: any) => {
                 <div className="col-sm-10"></div>
                 <div className="col-sm-5"></div>
                 <MDBCol md="4">
-                    <input className="form-control form-control-sm ml-3 w-75 float-right" type="text" placeholder="Search Booking" aria-label="Search"  onKeyDown={(e) => searchId(e)} />
+                    <input className="form-control form-control-sm ml-3 w-75 float-right" type="text" placeholder="Search Booking" aria-label="Search"  onKeyDown={(e) => searchId(e)} onChange={(e)=>{setBookingId(e.target.value)}}/>
                 </MDBCol>
             </div>
             <Modal show={show}
                 size="lg"
                 centered
             >
+                {/* <SearchBookingModal booking_id={bookingId}/> */}
                 <Modal.Footer>
                     <Button variant="danger" onClick={handleClose}>
                         Cancel
