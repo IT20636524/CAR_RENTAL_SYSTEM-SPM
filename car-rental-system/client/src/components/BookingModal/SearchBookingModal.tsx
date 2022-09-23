f7cc44dfe593989117dbba484ad24fd9cd96adc7
PrathibhaSathyanjalee <IT20636524@my.sliit.lk>
2022-09-24 00:16:25 +0530

    Search Booking Model Box - Connect to Backend

--- car-rental-system/client/src/components/BookingModal/SearchBookingModal.tsx
+++ car-rental-system/client/src/components/BookingModal/SearchBookingModal.tsx
@@ -1,8 +1,38 @@
+import axios from 'axios';
 import { MDBCol } from 'mdb-react-ui-kit'
-import React from 'react'
+import React, { useState } from 'react'
 import { Col, Form, Modal, Row } from 'react-bootstrap'
 
 export default function SearchBookingModal() {
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+    const [show, setShow] = useState(false);
+
+    const PF = "http://localhost:5000/image/"
+    const [booking_id,setBookingId] =useState("");
+    const [name, setName] = useState("");
+    const [address, setAddress] = useState("");
+    const [contact_number, setContactNumber] = useState("");
+    const [type_of_service, setTypeOfService] = useState("");
+    const [selected_model, setSelectedModel] = useState("");
+    const [no_of_days, setNoOfDays] = useState("");
+    const [location, setLocation] = useState("");
+    const [vehicle_pic, setVehiclePic]=useState("");
+
+
+    axios.get("http://localhost:5000/api/bookings/B002")
+    .then(function(response){
+        setBookingId(response.data['booking_id']);
+        setName(response.data['name']);
+        setAddress(response.data['address']);
+        setContactNumber(response.data['contact_number']);
+        setTypeOfService(response.data['type_of_service']);
+        setSelectedModel(response.data['selected_model']);
+        setNoOfDays(response.data['no_of_days']);
+        setLocation(response.data['location']);
+        setShow(true);
+    })
+
     return (
         <>
             <>
@@ -35,7 +65,7 @@ export default function SearchBookingModal() {
                                 Booking id
                             </Form.Label>
                             <Col sm="7">
-                                <Form.Control type="text" placeholder="Booking Id "  />
+                                <Form.Control type="text" placeholder="Booking Id " value={booking_id} />
                             </Col>
                         </Form.Group>
 
@@ -44,7 +74,7 @@ export default function SearchBookingModal() {
                                 Name
                             </Form.Label>
                             <Col sm="7">
-                                <Form.Control type="text" placeholder="Name" />
+                                <Form.Control type="text" placeholder="Name" value={name}/>
                             </Col>
                         </Form.Group>
 
@@ -53,7 +83,7 @@ export default function SearchBookingModal() {
                                 Address
                             </Form.Label>
                             <Col sm="7">
-                                <Form.Control type="text" placeholder="Address"  />
+                                <Form.Control type="text" placeholder="Address" value={address} />
                             </Col>
                         </Form.Group>
 
@@ -62,7 +92,7 @@ export default function SearchBookingModal() {
                                 Contact Number
                             </Form.Label>
                             <Col sm="7">
-                                <Form.Control type="text" placeholder="Contact Number" maxLength={10} />
+                                <Form.Control type="text" placeholder="Contact Number" maxLength={10} value={contact_number}/>
                             </Col>
                         </Form.Group>
 
@@ -72,7 +102,7 @@ export default function SearchBookingModal() {
                                 Type Of Service
                             </Form.Label>
                             <Col sm="7">
-                                <Form.Control type="text" placeholder="Type Of Service"  />
+                                <Form.Control type="text" placeholder="Type Of Service" value={type_of_service} />
                             </Col>
                         </Form.Group>
 
@@ -82,7 +112,7 @@ export default function SearchBookingModal() {
                                 Selected Model
                             </Form.Label>
                             <Col sm="7">
-                                <Form.Control type="text" placeholder="Selected Model"/>
+                                <Form.Control type="text" placeholder="Selected Model" value={selected_model}/>
                             </Col>
                         </Form.Group>
 
@@ -91,7 +121,7 @@ export default function SearchBookingModal() {
                                 No of Days
                             </Form.Label>
                             <Col sm="7">
-                                <Form.Control type="text" placeholder="No of Days" />
+                                <Form.Control type="text" placeholder="No of Days" value={no_of_days}/>
                             </Col>
                         </Form.Group>
 
@@ -100,7 +130,7 @@ export default function SearchBookingModal() {
                                 Location
                             </Form.Label>
                             <Col sm="7">
-                                <Form.Control type="text" placeholder="Location"/>
+                                <Form.Control type="text" placeholder="Location" value={location}/>
                             </Col>
                         </Form.Group>
 
