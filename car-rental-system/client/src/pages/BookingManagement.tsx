469218c249fb16221198ffa5ec117a638781b08e
PrathibhaSathyanjalee <IT20636524@my.sliit.lk>
2022-10-06 23:57:26 +0530

    Admin Booking-Booking table- Connect to Backend

--- car-rental-system/client/src/pages/BookingManagement.tsx
+++ car-rental-system/client/src/pages/BookingManagement.tsx
@@ -1,10 +1,39 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+
+interface bookings {
+    booking_id: string,
+    name: string,
+    email: string,
+    address: string,
+    contact_number: string,
+    type_of_service: string,
+    selected_model: string,
+    no_of_days: string,
+    location: string,
+    vehicle_pic: string,
+    payment_status: string
+}
 
 export default function BookingManagement() {
+    const [bookings, setBookings] = useState<bookings[]>([]);
+    useEffect(() => {
+        getData()
+    }, []);
+
+    const getData = () => {
+
+        axios.get("http://localhost:5000/api/bookings").then(res => {
+            console.log(res.data);
+            setBookings(res.data);
+        }).catch(error => {
+            console.log(error);
+        })
+    }
     return (
-        <>
-            <table className="table align-middle mb-0 bg-white">
-                <thead className="bg-light">
+        <div>
+            <table className="table align-middle mb-0 bg-white admin-booking-table">
+                <thead className="table-dark">
                     <tr>
                         <th>Vehicle</th>
                         <th>BookingId</th>
@@ -16,6 +45,64 @@ export default function BookingManagement() {
                         <th>Location</th>
                         <th>Payment Status</th>
                         <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {/* <tr> */}
+                        {bookings.map(bm => (
+                            <tr key={bm.booking_id} className='table-secondary'>
+                            <td >
+                                <div className="d-flex align-items-center">
+                                    <img
+                                        src="https://mdbootstrap.com/img/new/avatars/8.jpg"
+                                        alt=""
+                                        style={{ width: "45px", height: "45px" }}
+                                        className="rounded-circle"
+                                    />
+                                    <div className="ms-3">
+                                        <p className="fw-bold mb-1">{bm.selected_model}</p>
+                                        <p className="text-muted mb-0">{bm.email}</p>
+                                    </div>
+                                </div>
+                            </td>
+                            <td>{bm.booking_id}</td>
+                            <td>{bm.name}</td>
+                            <td>{bm.address}</td>
+                            <td>{bm.contact_number}</td>
+                            <td>{bm.type_of_service}</td>
+                            <td>{bm.no_of_days}</td>
+                            <td>{bm.location}</td>
+                            <td align='center'>
+                                {bm.payment_status=='paid'?<span className="badge rounded-pill bg-success d-inline">{bm.payment_status}</span> : <span className="badge rounded-pill bg-danger  d-inline">{bm.payment_status}</span>}
+                                
+                            </td>
+                            <td>
+                                <button
+                                    type="button"
+                                    className="btn btn-link btn-rounded btn-sm fw-bold"
+                                    data-mdb-ripple-color="dark"
+                                >
+                                    Accept
+                                </button>
+                            </td>
+                            </tr>
+                        ))}
+                </tbody>
+            </table>
+            <br />
+
+            {/* Accepted Bookings Table */}
+            <table className="table align-middle mb-0 bg-white">
+                <thead className="bg-light">
+                    <tr>
+                        <th>Vehicle</th>
+                        <th>BookingId</th>
+                        <th>Name</th>
+                        <th>Address</th>
+                        <th>Contact Number</th>
+                        <th>Type of Servie</th>
+                        <th>No of days</th>
+                        <th>Location</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -41,18 +128,6 @@ export default function BookingManagement() {
                         <td>With Driver</td>
                         <td>2</td>
                         <td>Kandy</td>
-                        <td>
-                            <span className="badge badge-success rounded-pill d-inline">Paid</span>
-                        </td>
-                        <td>
-                            <button
-                                type="button"
-                                className="btn btn-link btn-rounded btn-sm fw-bold"
-                                data-mdb-ripple-color="dark"
-                            >
-                                Accept
-                            </button>
-                        </td>
                     </tr>
                     <tr>
                         <td>
@@ -76,18 +151,7 @@ export default function BookingManagement() {
                         <td>Self Drive</td>
                         <td>3</td>
                         <td>Anuradhapura</td>
-                        <td>
-                            <span className="badge badge-warning rounded-pill d-inline">Awaiting</span>
-                        </td>
-                        <td>
-                            <button
-                                type="button"
-                                className="btn btn-link btn-rounded btn-sm fw-bold"
-                                data-mdb-ripple-color="dark"
-                            >
-                                Accept
-                            </button>
-                        </td>
+
                     </tr>
                     <tr>
                         <td>
@@ -111,108 +175,9 @@ export default function BookingManagement() {
                         <td>With Driver</td>
                         <td>2</td>
                         <td>Kandy</td>
-                        <td>
-                            <span className="badge badge-warning rounded-pill d-inline">Awaiting</span>
-                        </td>
-                        <td>
-                            <button
-                                type="button"
-                                className="btn btn-link btn-rounded btn-sm fw-bold"
-                                data-mdb-ripple-color="dark"
-                            >
-                                Accept
-                            </button>
-                        </td>
                     </tr>
                 </tbody>
             </table>
-            <br />
-            <table className="table align-middle mb-0 bg-white">
-                <thead className="bg-light">
-                    <tr>
-                        <th>Vehicle</th>
-                        <th>BookingId</th>
-                        <th>Name</th>
-                        <th>Address</th>
-                        <th>Contact Number</th>
-                        <th>Type of Servie</th>
-                        <th>No of days</th>
-                        <th>Location</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>
-                            <div className="d-flex align-items-center">
-                                <img
-                                    src="https://mdbootstrap.com/img/new/avatars/8.jpg"
-                                    alt=""
-                                    style={{ width: "45px", height: "45px" }}
-                                    className="rounded-circle"
-                                />
-                                <div className="ms-3">
-                                    <p className="fw-bold mb-1">Toyota</p>
-                                    <p className="text-muted mb-0">john.doe@gmail.com</p>
-                                </div>
-                            </div>
-                        </td>
-                        <td>B001</td>
-                        <td>John</td>
-                        <td>Colombo</td>
-                        <td>0112345698</td>
-                        <td>With Driver</td>
-                        <td>2</td> 
-                        <td>Kandy</td>   
-                    </tr>
-                    <tr>
-                        <td>
-                            <div className="d-flex align-items-center">
-                                <img
-                                    src="https://mdbootstrap.com/img/new/avatars/6.jpg"
-                                    className="rounded-circle"
-                                    alt=""
-                                    style={{ width: "45px", height: "45px" }}
-                                />
-                                <div className="ms-3">
-                                    <p className="fw-bold mb-1">WagonR</p>
-                                    <p className="text-muted mb-0">alex.ray@gmail.com</p>
-                                </div>
-                            </div>
-                        </td>
-                        <td>B002</td>
-                        <td>David</td>
-                        <td>Colombo</td>
-                        <td>0112354879</td>
-                        <td>Self Drive</td>
-                        <td>3</td>
-                        <td>Anuradhapura</td> 
-                        
-                    </tr>
-                    <tr>
-                        <td>
-                            <div className="d-flex align-items-center">
-                                <img
-                                    src="https://mdbootstrap.com/img/new/avatars/7.jpg"
-                                    className="rounded-circle"
-                                    alt=""
-                                    style={{ width: "45px", height: "45px" }}
-                                />
-                                <div className="ms-3">
-                                    <p className="fw-bold mb-1">Vezel</p>
-                                    <p className="text-muted mb-0">kate.hunington@gmail.com</p>
-                                </div>
-                            </div>
-                        </td>
-                        <td>B003</td>
-                        <td>John</td>
-                        <td>Colombo</td>
-                        <td>0112345698</td>
-                        <td>With Driver</td>
-                        <td>2</td>  
-                        <td>Kandy</td>    
-                    </tr>
-                </tbody>
-            </table>
-        </>
+        </div>
     )
 }
