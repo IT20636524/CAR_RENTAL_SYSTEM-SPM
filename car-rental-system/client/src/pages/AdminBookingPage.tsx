34438f3a38750dd33cbb3faa440419f60dfb8ef1
PrathibhaSathyanjalee <IT20636524@my.sliit.lk>
2022-10-09 20:47:29 +0530

    Update CSS-Admin Booking

--- car-rental-system/client/src/pages/AdminBookingPage.tsx
+++ car-rental-system/client/src/pages/AdminBookingPage.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import AcceptedBookings from '../components/AcceptedBookings'
 import AdminFooter from '../components/AdminFooter'
 import AdminHeader from '../components/AdminHeader'
 import AdminSideNav from '../components/AdminSideNav'
+import GenerateReportBooking from '../components/GenerateReportBooking'
 import SearchBooking from '../components/SearchBooking'
-import BookingManagement from './BookingManagement'
+import BookingManagement from '../components/BookingManagement'
 
 export default function AdminBookingPage() {
   return (
@@ -12,9 +14,14 @@ export default function AdminBookingPage() {
         <div className="content">
             <AdminHeader/>
             <br/>
+            <GenerateReportBooking/>
             <SearchBooking/>
             <br/>
             <BookingManagement/>
+            <br/>
+
+            <br/>
+            <AcceptedBookings/>
             <AdminFooter/>
         </div>
         
